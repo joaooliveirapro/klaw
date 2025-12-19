ca2ab334e4513678ed7b17e9897e8031eef0d3da
joaooliveirapro <joao00alex@gmail.com>
2025-12-19 11:55:53 +0000

    updated issue #11

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 // TODO: (#10:open) implement issue body parsing
 // TODO: (#12:open) implement ignore-todo keyword for todos that can be ignored
 // TODO: (#13:open) implement closing todos with the CLI (and it reflecting on the Github issue)
-// TODO: (#14:open) implement optional skip closed todos logic when listing all
-// TODO: (#16:open) implement config file for extra features (keyword, defaults, skips, etc.)
+// TODO: (#14:closed) implement optional skip closed todos logic when listing all
+// TODO: (#16:closed) implement config file for extra features (keyword, defaults, skips, etc.)
 // TODO: (#17:open) implement actions table: list all actions that can be commited (create or update certain tickets)
-// TODO: (#18:open) implement flag output (so users know what is set)
+// TODO: (#18:closed) implement flag output (so users know what is set)
 func main() {
 	printLogo()
 
@@ -22,7 +22,7 @@ func main() {
 	}
 
 	m := NewManager(args, cfg)
-	if !m.Args.Offline { // TODO: (#11:open) implement allow offline (no-git) checks for comments
+	if !m.Args.Offline { // TODO: (#11:closed) implement allow offline (no-git) checks for comments
 		repo, err := CheckGitRepo(m)
 		if err != nil {
 			log.Fatal(err)
