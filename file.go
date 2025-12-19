dff61df3dc7da318e2b9eb24b21a74ea263a93aa
joaooliveirapro <joao00alex@gmail.com>
2025-12-19 10:56:50 +0000

    refactored to account for excluded folders; added extra comments

--- file.go
+++ file.go
@@ -1,28 +1,48 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"io/fs"
 	"log"
 	"os"
-	"path/filepath"
 	"regexp"
 	"strings"
 )
 
+// Finds all todos in the src files that match extension in the directory
+func FindAllTodosInDirectory(m *Manager) []*Todo {
+	// TODO: (#8:open) implement logic to cover multiple extensions
+	localTodos := []*Todo{}
+	// Directories to exclude from search
+	excluded := make(map[string]int)
+	for _, f := range m.Config.ExcludeFolders {
+		excluded[f] = 1
+	}
+	for _, ext := range m.Config.Extensions {
+		files := findFilesByExtension(m.Config.Directory, ext, excluded)
+		for _, f := range files {
+			err := f.findTodosInFile(m.Config.TodoCommentSymbol, m.Config.TodoKeyword)
+			if err != nil {
+				log.Println(err) // Not a critical error if line number isn't found
+				return nil
+			}
+			if len(f.Todos) == 0 {
+				continue
+			}
+			localTodos = append(localTodos, f.Todos...)
+		}
+	}
+	return localTodos
+}
+
 // Checks file and loads all todos found within
-func (f *File) FindTodosInFile(m *Manager) error {
+func (f *File) findTodosInFile(todoCommentSymbol, todoKeyword string) error {
 	fileContent, err := ReadFile(f.Path)
 	if err != nil {
 		return err
 	}
-
-	todoRgx := `(?i)` + regexp.QuoteMeta(m.Config.TodoCommentSymbol) +
-		`\s*` + regexp.QuoteMeta(m.Config.TodoKeyword) +
+	todoRgx := `(?i)` + regexp.QuoteMeta(todoCommentSymbol) +
+		`\s*` + regexp.QuoteMeta(todoKeyword) +
 		`:\s*(\(\#(\d+):(\w+)\))?(.*)`
-
 	rgx := regexp.MustCompile(todoRgx)
 	matches := rgx.FindAllStringSubmatch(fileContent, -1) // -1: no limit
 	for _, m := range matches {
@@ -51,29 +71,9 @@ func (f *File) FindTodosInFile(m *Manager) error {
 	return nil
 }
 
-// Finds all todos in the src files that match extension in the directory
-func FindAllTodosInDirectory(m *Manager) []*Todo {
-	// TODO: (#8:open) implement logic to cover multiple extensions
-	localTodos := []*Todo{}
-	for _, ext := range m.Config.Extensions {
-		files := findFilesByExtension(ext, m)
-		for _, f := range files {
-			err := f.FindTodosInFile(m)
-			if err != nil {
-				log.Println(err) // Not a critical error if line number isn't found
-				return nil
-			}
-			if len(f.Todos) == 0 {
-				continue
-			}
-			localTodos = append(localTodos, f.Todos...)
-		}
-	}
-	return localTodos
-}
-
 // Rewrites the state (#id:state) of a todo to the new state found in the
-// twin GitHub issue
+// twin GitHub issue; Example: when an issue is closed it updated the local
+// todos state to closed;
 func RewriteTodoState(todo *Todo, issue *GitIssue) error {
 	content, err := ReadFile(todo.File)
 	if err != nil {
@@ -83,6 +83,7 @@ func RewriteTodoState(todo *Todo, issue *GitIssue) error {
 	// KEYWORD: (#123:closed) todotext
 	oldtodostr := fmt.Sprintf("(#%s:%s) %s", todo.IssueNumber, todo.IssueState, todo.Text) // todo contains old values
 	newtodostr := fmt.Sprintf("(#%d:%s) %s", issue.Number, issue.State, todo.Text)         // issue contains updated values
+	// to ensure replaceAll works properly, the newtodostr text has to be todo.Text
 	newcontent := strings.ReplaceAll(content, oldtodostr, newtodostr)
 	err = os.WriteFile(todo.File, []byte(newcontent), 0644)
 	if err != nil {
@@ -92,11 +93,11 @@ func RewriteTodoState(todo *Todo, issue *GitIssue) error {
 	padded := fmt.Sprintf("%-17s", s) // pad based on visible chars
 	fmt.Printf("%s (%s:%s) %s %s\n",
 		C(padded, Cyan), C(fmt.Sprintf("#%d", issue.Number), Yellow), issue.State,
-		issue.Title, C("updated", Green))
+		todo.Text, C("updated", Green))
 	return nil
 }
 
-// Rewrutes the untracekd todo with the corresponding repr of its twin
+// Rewrites the untracekd todo with the corresponding repr of its twin
 // GitHub issue, including (#issuenumber:state) original text
 func RewriteUntrackedTodo(todo *Todo, issue *GitIssue) error {
 	// KEYWORD: todotext
@@ -117,63 +118,4 @@ func RewriteUntrackedTodo(todo *Todo, issue *GitIssue) error {
 		C(padded, Cyan), C(fmt.Sprintf("#%d", issue.Number), Yellow), issue.State,
 		issue.Title, C("updated", Green))
 	return nil
-}
-
-/* Utils */
-
-// Read file and return its content
-func ReadFile(filepath string) (string, error) {
-	f, err := os.Open(filepath)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-	contentB, err := io.ReadAll(f)
-	if err != nil {
-		return "", err
-	}
-	return string(contentB), nil
-}
-
-// Find files that match extension
-func findFilesByExtension(extenstion string, m *Manager) []*File {
-	filesToCheck := []*File{}
-	excluded := make(map[string]int)
-	for _, f := range m.Config.ExcludeFolders {
-		excluded[f] = 1
-	}
-	filepath.Walk(m.Config.Directory, func(path string, info fs.FileInfo, err error) error {
-		if err != nil {
-			return nil
-		}
-		if info.IsDir() {
-			if _, skip := excluded[info.Name()]; skip {
-				return filepath.SkipDir // signals .Walk to skip this directory
-			}
-		}
-		if strings.HasSuffix(info.Name(), extenstion) {
-			filesToCheck = append(filesToCheck, &File{Path: path})
-		}
-		return nil
-	})
-	return filesToCheck
-}
-
-// Find the line number where substr occurs in the file
-func findLineNumber(filepath, text string) (int, error) {
-	f, err := os.Open(filepath)
-	if err != nil {
-		return -1, err
-	}
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	lineNum := 1
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, strings.TrimSpace(text)) {
-			return lineNum, nil
-		}
-		lineNum++
-	}
-	return -1, nil
 }
