d830c0dd72004075068316e33713a6de2159a55d
joaooliveirapro <joao00alex@gmail.com>
2025-12-19 10:00:50 +0000

    main entry; refactored to use appmanager

--- main.go
+++ main.go
@@ -1,35 +1,38 @@
 package main
 
+import (
+	"log"
+)
+
+// TODO: (#10:open) implement issue body parsing
+// TODO: (#12:open) implement ignore-todo keyword for todos that can be ignored
+// TODO: (#13:open) implement closing todos with the CLI (and it reflecting on the Github issue)
+// TODO: (#14:open) implement optional skip closed todos logic when listing all
+// TODO: (#16:open) implement config file for extra features (keyword, defaults, skips, etc.)
+// TODO: (#17:open) implement actions table: list all actions that can be commited (create or update certain tickets)
+// TODO: (#18:open) implement flag output (so users know what is set)
 func main() {
 	printLogo()
-	// Parse args
-	args, err := ParseArgs()
+
+	args := ParseArgs()
+
+	cfg, err := ParseConfigFile()
 	if err != nil {
-		printErr(err)
-		return
+		log.Fatal(err)
 	}
-	// Check .git folder exists in directory
-	repo, err := CheckGitfolder(args.Directory)
-	if err != nil {
-		printErr(err)
-		return
+
+	m := NewManager(args, cfg)
+	if !m.Args.Offline { // TODO: (#11:open) implement allow offline (no-git) checks for comments
+		repo, err := CheckGitRepo(m)
+		if err != nil {
+			log.Fatal(err)
+		}
+		m.AssignRepo(repo)
 	}
-	// Check .gittoken file exists
-	token, err := LoadGitToken()
-	if err != nil {
-		printErr(err)
-		return
+	if m.Args.ListAll {
+		HandleListAll(m)
 	}
-	// Get all local todos
-	localTodos, err := FindAllTodosInDirectory(args.Directory, args.Extension)
-	if err != nil {
-		printErr(err)
-		return
-	}
-	if args.ListAll {
-		handleListAll(args, repo, localTodos, token)
-	}
-	if args.Create {
-		handleCreate(repo, localTodos, token)
+	if m.Args.Create {
+		HandleCreate(m)
 	}
 }
